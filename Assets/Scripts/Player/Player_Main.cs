25e0e4c4abef35bb9ef32d23a365bde4fbaf2a46
Rafael Jordani Coelho <rafitapc08@gmail.com>
2026-05-26 11:22:33 -0300

    Cena inGame pre definida
    Movimentação do personagem feita por completo, ele ainda esta sem a parte visual...
    Movi da camera feita segue o personagem e fica fixa no eixo Y.
    
    Codigo 100% documentado

--- Assets/Scripts/Player/Player_Main.cs
+++ Assets/Scripts/Player/Player_Main.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 public class Player_Main : MonoBehaviour
 {
     public Rigidbody2D Rgdb2D;
-    public float Vel;
+    public float Vel, JumpForce;
     public Vector2 Dir;
     public GameObject Camera;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool OnFloor;
     void Start()
     {
         
@@ -16,9 +15,50 @@ public class Player_Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Rgdb2D.linearVelocity = Dir * Vel;
+        Dir = new Vector2(Input.GetAxisRaw("Horizontal"),0);
+        //Aqui voce libera o persoangem so andar horizontal e deixando o eixo Y em 0 sempre
 
-        Camera.transform.position = Rgdb2D.position ;
+        Rgdb2D.linearVelocity = new Vector2(Dir.x * Vel, Rgdb2D.linearVelocity.y);
+        //Aqui voce adiciona a força de andar no Rg mas apenas no eixo X por que a logica precisa ser assim mantendo o eixo Y para o addforce fazer ele pular  
+
+
+
+        CameraMov();
+        //Mov da camera
+        Pulo();
+        //Pulo do pernagem 
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            OnFloor = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            OnFloor = false;
+        }
+    }
+
+    public void CameraMov()
+    {
+        Camera.transform.position = new Vector3(this.gameObject.transform.position.x, 0, -10);
+        //Isso faz com que a camera siga o personagem no eixo X e nao saia do eixo Y que sempre vai ser fixo em 0 
+    }
+
+    public void Pulo()
+    {
+        if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.W)) && (OnFloor == true)) 
+        {
+            Rgdb2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            // Aqui ele adiciona forca ao pulo mas apenas a eixo Y e o forcemode2d é para o objeto ser mais interagivel com massa e gravidade
+        }
     }
 }
