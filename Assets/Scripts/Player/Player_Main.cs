313467fd3742a3ff91eafb557794dca0843da197
Rafael Jordani Coelho <rafitapc08@gmail.com>
2026-09-08 16:57:36 -0300

    feture/refact and anim

--- Assets/Scripts/Player/Player_Main.cs
+++ Assets/Scripts/Player/Player_Main.cs
@@ -2,35 +2,37 @@ using UnityEngine;
 
 public class Player_Main : MonoBehaviour
 {
-    public Rigidbody2D Rgdb2D;
-    public float Vel, JumpForce;
-    public Vector2 dir;
-    public GameObject Camera;
+    //Personagem geral
     public bool OnFloor;
+    //Variaveis boolianas do personagem 
+
+    public float Vel;
+    public float JumpForce;
+    //Variaveis floats do personagem 
+
+    //Movimento do personagem
+    public Vector2 dir;
+    public Rigidbody2D Rgdb2D;
+
+    //Camera do personagem
+    public GameObject Camera;
+
+    //Animação do personagem
     public Animator Anim;
     public SpriteRenderer spriteRenderer;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        dir = new Vector2(Input.GetAxisRaw("Horizontal"),0);
-        //Aqui voce libera o persoangem so andar horizontal e deixando o eixo Y em 0 sempre
-
-        Rgdb2D.linearVelocity = new Vector2(dir.x * Vel, Rgdb2D.linearVelocity.y);
-        //Aqui voce adiciona a força de andar no Rg mas apenas no eixo X por que a logica precisa ser assim mantendo o eixo Y para o addforce fazer ele pular  
-        Debug.Log(dir.x);
-
-
+        Mov();
+        //Mov do personagem
         CameraMov();
         //Mov da camera
         Pulo();
         //Pulo do pernagem 
         animWalk();
         //Animação do personagem andando
+        animJump();
+        //Animaçao de pulo do personagem
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -40,7 +42,6 @@ public class Player_Main : MonoBehaviour
             OnFloor = true;
         }
     }
-
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Floor"))
@@ -48,13 +49,20 @@ public class Player_Main : MonoBehaviour
             OnFloor = false;
         }
     }
-
     public void CameraMov()
     {
         Camera.transform.position = new Vector3(this.gameObject.transform.position.x, 0, -10);
         //Isso faz com que a camera siga o personagem no eixo X e nao saia do eixo Y que sempre vai ser fixo em 0 
     }
+    public void Mov()
+    {
+        dir = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        //Aqui voce libera o persoangem so andar horizontal e deixando o eixo Y em 0 sempre
 
+        Rgdb2D.linearVelocity = new Vector2(dir.x * Vel, Rgdb2D.linearVelocity.y);
+        //Aqui voce adiciona a força de andar no Rg mas apenas no eixo X por que a logica precisa ser assim mantendo o eixo Y para o addforce fazer ele pular  
+        Debug.Log(dir.x);
+    }
     public void Pulo()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
@@ -71,7 +79,6 @@ public class Player_Main : MonoBehaviour
 
         }
     }
-
     public void animWalk()
     {
         if (dir.x == 1)
@@ -89,5 +96,15 @@ public class Player_Main : MonoBehaviour
             Anim.SetBool("isWalk", false);
         }
     }
-
+    public void animJump()
+    {
+        if (OnFloor)
+        {
+            Anim.SetBool("isJump", false);
+        }
+        else
+        {
+            Anim.SetBool("isJump", true);
+        }
+    }
 }
